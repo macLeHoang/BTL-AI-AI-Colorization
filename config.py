c9e06f4dc7f25b231c3024d930365e332e0ed3b4
macLeHoang <92416926+macLeHoang@users.noreply.github.com>
2022-07-16 10:38:57 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+BATCH = 16 // batch size
+pretrained_epochs = 20
+epochs = 20
+
+pretrained_weight_path_ = '' // path to folder that saving pre-trained weights
+gen_weight_path_ = '' // path to folder that saving generator weights
+dis_weight_path_ = '' // path to folder that saving discriminator weights
