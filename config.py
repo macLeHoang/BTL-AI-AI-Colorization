49b9784f16ef655ed39e4b32309a3514ace82ff9
macLeHoang <92416926+macLeHoang@users.noreply.github.com>
2022-07-16 10:49:28 +0700

    Change your configuration here

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ BATCH = 16 // batch size
 pretrained_epochs = 20
 epochs = 20
 
+path_ = '' // path to your training data
+
 pretrained_weight_path_ = '' // path to folder that saving pre-trained weights
 gen_weight_path_ = '' // path to folder that saving generator weights
 dis_weight_path_ = '' // path to folder that saving discriminator weights
