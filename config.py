f5a579a246eb9415f3d435873e4979ba6e7b6795
macLeHoang <92416926+macLeHoang@users.noreply.github.com>
2022-07-16 11:00:52 +0700

    Change your configuration here

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ pretrained_weight_path_ = '' // path to folder that saving pre-trained weights
 gen_weight_path_ = '' // path to folder that saving generator weights
 dis_weight_path_ = '' // path to folder that saving discriminator weights
 
+logs = False // logging losses value during training or not
+logs_path = '' // Ignore this if logs is False
